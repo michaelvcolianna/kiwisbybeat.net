7a0e48377d318e76819cfc29aa468ef1869c6710
Michael V. Colianna <michael.colianna@me.com>
2024-02-22 16:16:54 -0500

    #3

--- cachimize.js
+++ cachimize.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-fs.readdir(`${__dirname}/../cache/.cache`, (err, files) => {
+fs.readdir(`${__dirname}/../cache`, (err, files) => {
   if(err) {
     console.log(err);
   }
