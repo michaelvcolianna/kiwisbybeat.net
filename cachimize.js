ce269aa8bd472992afeecaf60f28e7013494e174
Michael V. Colianna <michael.colianna@me.com>
2024-02-22 16:11:11 -0500

    give this a try

---
+++ cachimize.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+fs.readdir(`${__dirname}/../cache`, (err, files) => {
+  if(err) {
+    console.log(err);
+  }
+  else {
+    console.log("\nContents:");
+    files.forEach(file => {
+      console.log(`- ${file}`);
+    });
+  }
+});
+
+return 0;
