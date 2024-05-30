bcfe12c6efe4751c30fd8f56ea269fc5473233dc
Michael V. Colianna <michael.colianna@me.com>
2024-05-29 23:06:55 -0400

    Gets styles going - Installs SASS - Updates TypeScript config to resolve @styles dir - Updates Astro config to pass variables & mixins imports to Vite SASS - Ports Gatsby module styles to Astro scoped styles for elements on the homepage, adjusting as needed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "${__dirname}/src/styles/variables"; @import "${__dirname}/src/styles/mixins";`
+        }
+      }
+    }
+  }
+})
